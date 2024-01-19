ff1b459cde7f0f0763a37a0708a6cace07b74fc4
Oleksander Derevianko <derevianko.99@ukr.net>
2024-01-20 00:23:57 +0200

    added new page + new section

--- js/main.js
+++ js/main.js
@@ -9,3 +9,40 @@ setTimeout(() => {
 setTimeout(() => {
   share.classList.remove("hover");
 }, 3000);
+
+$('.education__slider').slick({
+    infinite: false,
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/arrow-left.svg"></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/arrow-right.svg"></button>',
+    slidesToShow: 1,
+    slidesToScroll: 1
+  
+  });
+
+  $('.education__slider').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    if (nextSlide === 0) {
+        $('.slick-prev').hide();
+    } else {
+        $('.slick-prev').show();
+    }
+
+    if (nextSlide === slick.slideCount - 1) {
+        $('.slick-next').hide();
+    } else {
+        $('.slick-next').show();
+    }
+});
+
+$('.education__slider').on('setPosition', function(event, slick) {
+    if (slick.currentSlide === 0) {
+        $('.slick-prev').hide();
+    } else {
+        $('.slick-prev').show();
+    }
+
+    if (slick.currentSlide === slick.slideCount - 1) {
+        $('.slick-next').hide();
+    } else {
+        $('.slick-next').show();
+    }
+});
